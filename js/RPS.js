d4087217d9b7492fe6045c3fae834c58b1e06430
soniawu <sonia.wu.0906@gmail.com>
2023-12-19 13:41:04 -0600

    Complete no-styling version of rock-paper-scissors
    Planning to do styles later.

--- js/RPS.js
+++ js/RPS.js
@@ -1,10 +1,47 @@
 
 
 const rps = ["rock", "paper", "scissors"];
-let computerChoice = "";     // computer choice for a game
+let computerChoice = "";    // computer choice for a game
 let playerChoice = "";       // player choice for a game
+
 let computerScore = 0;  // total game score for computer
 let playerScore = 0;    // total game score for player 
+
+//const scoreBoardAll = document.querySelector("#scoreBoard > p");
+const playerOptMsg = document.querySelector("#playerOptMsg");
+const computerOptMsg = document.querySelector("#computerOptMsg");
+const playerScoreMsg = document.querySelector("#playerScoreMsg");
+const computerScoreMsg = document.querySelector("#computerScoreMsg");
+const winnerMsg = document.querySelector("#winnerMsg");
+const completeMsg = document.querySelector("#completeMsg");
+ 
+// get the selection element
+let rock = document.querySelector("#rockBtn");
+let paper = document.querySelector("#paperBtn");
+let scissors = document.querySelector("#scissorsBtn");
+
+let fiveReach = true;
+
+rock.textContent = "rock";
+paper.textContent = "paper";
+scissors.textContent = "scissors";
+
+
+// Add event listeners for each button
+// This group could be improved by calling one function
+// Consider to change it later
+rock.addEventListener("click", () => {
+    initRound("rock");
+});
+
+paper.addEventListener("click", function() {
+    initRound("paper");
+});
+
+scissors.addEventListener("click", function() {
+    initRound("scissors");
+});
+
 
 /*  This function will get a random number 0-2 which are
     the index of rps[]. Use the number as computer's selection
@@ -13,37 +50,64 @@ function getComputerChoice()
 {
     let idx = Math.floor(Math.random()*3);
     return rps[idx];
+}
+
+function initScoreBoard() {
+    computerScore = 0;
+    playerScore = 0;
+    //scoreBoard.children.textContent = "";
+
+    // Clearing all messages in scoreBoard
+    // will find out if can be done all at parent node
+    document.querySelector("#computerOptMsg").textContent = "";
+    playerScoreMsg.textContent = ""; 
+    computerScoreMsg.textContent = "";
+    winnerMsg.textContent = ""; 
+    completeMsg.textContent = "";
+    
 
 }
 
-/*  This function will prompt to the player for entering 
-    player's selection.
+/* This function initialize a new round of game.
+    1) set player's 
+    to be continue
 */
-function getPlayerChoice()
-{
-    /*  prompt player to his/her choice
-        for the first version, html option is not yet implemented,
-        should verify if the choice is valid. If change to use html selection
-        later, no need to do the verification
-    */
-    return undefined;  // will return player selection when real codes are in
+function initRound(opt) {
+    if (fiveReach) {
+        initScoreBoard();
+        fiveReach = false;
+    }
+
+    playerChoice = opt;
+    computerChoice = getComputerChoice();
+    playRound(computerChoice, playerChoice);
 }
 
+/*  Core function of the game.
+    It takes both the player and computer selection as input,
+    determins who is the winner of this round.
+    update the scores and post the updated scores.
+*/
 function playRound(computerSelection, playerSelection)
 {
-    // convert all selections to lower case 
-    // although the computerSelection is taken out from the 
-    // array rps[], should be all lower cases, do it anyway
-
+   // convert all selections to lower case 
+   // although the computerSelection is taken out from the 
+   // array rps[], should be all lower cases, do it anyway
    let c_sel = computerSelection.toLowerCase();
    let p_sel = playerSelection.toLowerCase(); 
+
+   // clean the opt msg from last round
+   playerOptMsg.textContent = "";
+   computerOptMsg.textContent = "";
+   winnerMsg.textContent = "";
 
    // to avoid type the string, use var names for string
    let r = rps[0];  // r is "rock"
    let p = rps[1];  // p is "paper"
    let s = rps[2];  // s is "scissors"
 
-   console.log(`Your choice is ${p_sel}  Computet's choice is ${c_sel}`);
+   let winner = "";
+
 
    // start applying the rules
    // doing console.log in every case seams not
@@ -53,14 +117,16 @@ function playRound(computerSelection, playerSelection)
    {
     case r : switch (c_sel) {
         case r : // draw
-            console.log("Game draw this round.");
+            //console.log("Game draw this round.");
             break ;
         case p : // computer wins
-            console.log("You lose this round. Sorry!");
+            //console.log("You lose this round. Sorry!");
+            winner = "COMPUTER";
             computerScore++;
             break;
         case s : // player wins
-            console.log("You win this round. Congrats!");
+            //.log("You win this round. Congrats!");
+            winner = "YOU"
             playerScore++;
             break; 
         default :
@@ -69,14 +135,16 @@ function playRound(computerSelection, playerSelection)
 
     case p : switch (c_sel) {
         case r : // player wins
-            console.log("You win this round. Congrats!");
+            //.log("You win this round. Congrats!");
+            winner = "YOU";
             playerScore++;
             break;
         case p : // draw
-            console.log("Game draw this round.");
+            //console.log("Game draw this round.");
             break ;
         case s : // computer wins
-            console.log("You lose this round. Sorry!");
+            //console.log("You lose this round. Sorry!");
+            winner = "COMPUTER";
             computerScore++;
             break; 
         default :
@@ -85,15 +153,17 @@ function playRound(computerSelection, playerSelection)
 
     case s : switch (c_sel) {
         case r : // computer wins
-            console.log("You lose this round. Sorry!");
+            //console.log("You lose this round. Sorry!");
+            winner = "COMPUTER";
             computerScore++;
             break;
         case p : // player wins
-            console.log("You win this round. Congrats!");
+            //console.log("You win this round. Congrats!");
+            winner = "YOU";
             playerScore++;
             break;
         case s : // draw
-            console.log("Game draw this round.");
+            //console.log("Game draw this round.");
             break ; 
         default :
 
@@ -102,50 +172,23 @@ function playRound(computerSelection, playerSelection)
     default :
    }
    
+   playerOptMsg.textContent = "Your option is : " + playerSelection;
+   computerOptMsg.textContent = "Computer's option is : " + computerSelection;
+   playerScoreMsg.textContent = "Your score is : " + `${playerScore}`;
+   computerScoreMsg.textContent = "Computer's score is : " + `${computerScore}`;
+
+   if (winner === "") {
+    winnerMsg.textContent = "It is a draw game, no winner for this round. ";
+   } else {
+    winnerMsg.textContent = "The winner is " + winner + " for this round";
+   }
+
+   // If the player or computer reaches 5 points, post message and start new game.
+   if (computerScore>=5) {
+    completeMsg.textContent = "Computer wins 5 rounds. Will start new games.";
+    fiveReach = true;
+   } else if (playerScore>=5) {
+    completeMsg.textContent = "Congrats, you win 5 rounds. Will start new games";
+    fiveReach = true;
+   }
 }
- 
-    /*
-    loop the following 5 times
-        - assign computerChoice by calling getComputerChoice()
-        - assign playerChoice by calling getPlayerChoice()
-        - call playRound(computerChoice, playerChoice)
-    - check computerScore and playerScore to determine the winner
-    - announce the winner
-    - no return for the function
-    */
-
-    
-    for (let i = 1; i <= 5; i++) {
-        // get player choice
-        let correct = false;
-        do {
-            playerChoice = prompt("Enter 'rock' or 'paper' or 'scissors' for this round: ");
-            let choice = playerChoice.toLowerCase();
-            if ((choice === "rock") || (choice === "paper") || (choice === "scissors")) {
-                correct = true; } 
-                else {
-                    console.log("Please enter a valid option.");
-                }
-        } while (!correct);
-        computerChoice = getComputerChoice(); 
-        playRound(computerChoice, playerChoice);
-        console.log(`Current scores - You : ${playerScore}  Computer : ${computerScore}\n\n`);
-    }
-    
-
-    let winner;
-    if (playerScore > computerScore ) {
-        winner = "YOU";
-    } else 
-    if (computerScore > playerScore)
-    {
-        winner = "COMPUTER";
-    }
-
-    console.log(`Final scores - You : ${playerScore}  Computer : ${computerScore}\n\n`);
-    if (winner === undefined){
-        console.log("Your score is same as computer's. No winner of this game. ");
-    } else
-    {
-        console.log(`The winner is ${winner}!`);
-    }
